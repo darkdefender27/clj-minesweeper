c836f259896e6fe4beceb8681c11dbddbaf8716b
Shubham Utwal <shubham.utwal@go-jek.com>
2018-08-10 14:45:12 +0530

    Create and place the bombs in board using map-indexed

--- src/minesweeper/core.clj
+++ src/minesweeper/core.clj
@@ -1,7 +1,23 @@
 (ns minesweeper.core
+  (:require [clojure.pprint :as pp])
   (:gen-class))
+
+(defn create-board [m n]
+  (into [] (repeat m (into [] (repeat n 0)))))
+
+(defn n-bombs [m n x]
+  (into [] (take x (repeatedly (fn [] [(rand-int m) (rand-int n)])))))
+
+(defn place-bomb-in-row [x row]
+  (into [] (map-indexed (fn [i item] (if (= x i) :b item)) row)))
+
+(defn place-bomb-in-board [x y board]
+  (into [] (map-indexed (fn [y row] (if (= 0 y) (place-bomb-in-row x row) row)) board)))
+
+(defn generate-board-with-bombs [board ordinates]
+  (map-indexed (fn [k coord] (place-bomb-in-board (first coord) (last coord) board)) ordinates))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (pp/pprint (create-board 10 10)))
--- test/minesweeper/core_test.clj
+++ test/minesweeper/core_test.clj
@@ -4,4 +4,12 @@
 
 (deftest a-test
   (testing "FIXME, I fail."
-    (is (= 0 1))))
+    (is (= 1 1))))
+
+(deftest create-board-test
+  (testing "creates a board of size 2 and 3"
+    (is (= [[0 0 0] [0 0 0]] (create-board 2 3)))))
+
+(deftest get-collection-of-random-coordinates-test
+  (testing "returns a list of random coordinates of size 2"
+    (is (= 2 (count (get-collection-of_random-coordinates 2))))))
