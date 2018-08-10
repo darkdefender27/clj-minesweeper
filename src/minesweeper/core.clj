e5b8d3e98910a435781afca9bd6d7441567c2fae
Shubham Utwal <shubham.utwal@go-jek.com>
2018-08-10 19:39:36 +0530

    Neighbours received fine. TODO:// Bug in mines?

--- src/minesweeper/core.clj
+++ src/minesweeper/core.clj
@@ -15,9 +15,15 @@
   (into [] (map-indexed (fn [y row] (if (= 0 y) (place-bomb-in-row x row) row)) board)))
 
 (defn generate-board-with-bombs [board ordinates]
-  (map-indexed (fn [k coord] (place-bomb-in-board (first coord) (last coord) board)) ordinates))
+  (first (map-indexed (fn [k coord] (place-bomb-in-board (first coord) (last coord) board)) ordinates)))
+
+(defn within-bounds? [x y]
+  (and (and (>= x 0) (< x 10)) (and (>= y 0) (< y 10))))
+
+(defn get-neighbours [p q]
+    (for [x (range (dec p) (+ p 2)) y (range (dec q) (+ q 2)) :when (and (not= [p q] [x y]) (within-bounds? x y))] [x y]))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (pp/pprint (create-board 10 10)))
+  (pp/pprint (generate-board-with-bombs (create-board 10 10) (n-bombs 10 10 3))))
