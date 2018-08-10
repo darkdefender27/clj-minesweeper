530fd69880191bb6c963d678d9ef2207f77dfd90
Shubham Utwal <shubham.utwal@go-jek.com>
2018-08-10 22:46:02 +0530

    Fixed gen-board-with-bombs - Using reduce instead of map

--- src/minesweeper/core.clj
+++ src/minesweeper/core.clj
@@ -8,22 +8,21 @@
 (defn n-bombs [m n x]
   (into [] (take x (repeatedly (fn [] [(rand-int m) (rand-int n)])))))
 
-(defn place-bomb-in-row [x row]
-  (into [] (map-indexed (fn [i item] (if (= x i) :b item)) row)))
+(defn place-bomb-in-row [y row]
+  (into [] (map-indexed (fn [i item] (if (= y i) :b item)) row)))
 
 (defn place-bomb-in-board [x y board]
-  (into [] (map-indexed (fn [y row] (if (= 0 y) (place-bomb-in-row x row) row)) board)))
+  (into [] (map-indexed (fn [i row] (if (= x i) (place-bomb-in-row y row) row)) board)))
 
 (defn generate-board-with-bombs [board ordinates]
-  (first (map-indexed (fn [k coord] (place-bomb-in-board (first coord) (last coord) board)) ordinates)))
+  (reduce (fn [b locn] (assoc-in b locn :b)) board ordinates))
 
 (defn within-bounds? [x y]
   (and (and (>= x 0) (< x 10)) (and (>= y 0) (< y 10))))
 
 (defn get-neighbours [p q]
-    (for [x (range (dec p) (+ p 2)) y (range (dec q) (+ q 2)) :when (and (not= [p q] [x y]) (within-bounds? x y))] [x y]))
+  (for [x (range (dec p) (+ p 2)) y (range (dec q) (+ q 2)) :when (and (not= [p q] [x y]) (within-bounds? x y))] [x y]))
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
   (pp/pprint (generate-board-with-bombs (create-board 10 10) (n-bombs 10 10 3))))
--- test/minesweeper/core_test.clj
+++ test/minesweeper/core_test.clj
@@ -10,6 +10,26 @@
   (testing "creates a board of size 2 and 3"
     (is (= [[0 0 0] [0 0 0]] (create-board 2 3)))))
 
-(deftest get-collection-of-random-coordinates-test
+(deftest n-bombs-test
   (testing "returns a list of random coordinates of size 2"
-    (is (= 2 (count (get-collection-of_random-coordinates 2))))))
+    (is (= 2 (count (n-bombs 10 10 2))))))
+
+(deftest place-bomb-in-row-test
+  (let [row [1 2 3]]
+  (testing "returns a vec with bomb placed at 2"
+    (is (= [1 2 :b] (place-bomb-in-row 2 row))))))
+
+(deftest place-bomb-in-board-test
+  (let [board [[1 2]
+               [3 4]]]
+    (testing "returns the board with bomb placed at 0, 0"
+      (is (= [[:b 2] [3 4]] (place-bomb-in-board 0 0 board))))
+
+    (testing "returns the board with bomb placed at 1, 0"
+      (is (= [[1 2] [:b 4]] (place-bomb-in-board 1 0 board))))))
+
+(deftest generate-board-with-bombs-test
+  (let [board [[1 2 3][4 5 6][7 8 9]]
+        ordns [[1 1][1 2][2 0]]]
+    (testing "returns the board with all bombs placed as per ordns"
+      (is (= [[1 2 3][4 :b :b][:b 8 9]] (generate-board-with-bombs board ordns))))))
